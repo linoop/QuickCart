98621fb2ba97e057ffc497d13caedae194114482
Linoop Laxman <linoop1989@gmail.com>
2024-03-28 18:35:41 +0530

    test case modification

--- app/src/test/java/com/linoop/quickcart/cart/viewmodel/CartViewModelTest.kt
+++ app/src/test/java/com/linoop/quickcart/cart/viewmodel/CartViewModelTest.kt
@@ -1,36 +1,48 @@
 package com.linoop.quickcart.cart.viewmodel
 
-import com.google.common.truth.Truth
-import com.linoop.quickcart.cart.repository.CartRepositoryImpl
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import com.linoop.quickcart.cart.repository.CartRepository
 import com.linoop.quickcart.cart.usecase.DeleteFormCartUseCaseImpl
 import com.linoop.quickcart.cart.usecase.OpenCartUseCaseImpl
 import com.linoop.quickcart.main.model.Product
-import com.linoop.quickcart.main.storage.ProductDao
+import com.linoop.quickcart.utils.Resource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 
 @Suppress("DEPRECATION")
+@ExperimentalCoroutinesApi
 class CartViewModelTest {
 
-    private lateinit var dao: ProductDao
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    private val testScope = TestCoroutineScope(testDispatcher)
+
+    private lateinit var cartRepository: CartRepository
     private lateinit var cartViewModel: CartViewModel
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
-        Dispatchers.setMain(Dispatchers.Default)
-        dao = Mockito.mock(ProductDao::class.java)
-        val cartRepository = CartRepositoryImpl(dao)
+        Dispatchers.setMain(testDispatcher)
+        cartRepository = Mockito.mock(CartRepository::class.java)
         val openCartUseCase = OpenCartUseCaseImpl(cartRepository)
         val deleteFormCartUseCase = DeleteFormCartUseCaseImpl(cartRepository)
         cartViewModel = CartViewModel(openCartUseCase, deleteFormCartUseCase)
@@ -40,17 +52,18 @@ class CartViewModelTest {
     @After
     fun tearDown() {
         Dispatchers.resetMain()
+        testScope.cleanupTestCoroutines()
     }
 
     @Test
-    fun `test open cart`() = runBlocking {
+    fun `test open cart`() = testScope.runBlockingTest {
         val testData = listOf(
             Product(brand = "apple"),
             Product(brand = "samsung"),
         )
-        Mockito.`when`(dao.getAllProducts()).thenReturn(testData)
+        Mockito.`when`(cartRepository.getAllItems()).thenReturn(flowOf(Resource.Success(testData, "Success")))
         cartViewModel.openCart()
-        delay(1000)
-        Truth.assertThat(cartViewModel.openCartDataState.value.value.products).isEqualTo(testData)
+        advanceUntilIdle()
+        assertThat(cartViewModel.openCartDataState.value.value.products).isEqualTo(testData)
     }
 }
--- app/src/test/java/com/linoop/quickcart/product/viewmodel/ProductViewModelTest.kt
+++ app/src/test/java/com/linoop/quickcart/product/viewmodel/ProductViewModelTest.kt
@@ -9,12 +9,19 @@ import com.linoop.quickcart.product.repository.ProductRepositoryImpl
 import com.linoop.quickcart.product.usecase.AddToCartUseCaseImpl
 import com.linoop.quickcart.product.usecase.GetProductByIdUseCaseImpl
 import com.linoop.quickcart.main.storage.ProductDao
+import com.linoop.quickcart.product.repository.CartRepository
+import com.linoop.quickcart.product.repository.ProductRepository
 import com.linoop.quickcart.utils.ApiState
+import com.linoop.quickcart.utils.Resource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
@@ -26,50 +33,52 @@ import org.mockito.MockitoAnnotations
 import retrofit2.Response
 
 @Suppress("DEPRECATION")
+@ExperimentalCoroutinesApi
 class ProductViewModelTest {
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val testScope = TestCoroutineScope(testDispatcher)
 
     @Mock
-    lateinit var apiService: ApiService
+    private lateinit var productRepository: ProductRepository
 
     @Mock
-    lateinit var productDao: ProductDao
-
+    private lateinit var cartRepository: CartRepository
     private lateinit var productViewModel: ProductViewModel
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
-        Dispatchers.setMain(Dispatchers.Default)
+        Dispatchers.setMain(testDispatcher)
         productViewModel = ProductViewModel(
-            getProductById = GetProductByIdUseCaseImpl(ProductRepositoryImpl(apiService)),
-            addToCartUseCase = AddToCartUseCaseImpl(CartRepositoryImpl(productDao))
+            getProductById = GetProductByIdUseCaseImpl(productRepository),
+            addToCartUseCase = AddToCartUseCaseImpl(cartRepository)
         )
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @After
     fun tearDown() {
         Dispatchers.resetMain()
+        testScope.cleanupTestCoroutines()
     }
 
     @Test
-    fun `test get product by ID`() = runBlocking {
-        `when`(apiService.getProductById(1)).thenReturn(Response.success(Product(brand = "abc")))
+    fun `test get product by ID`() = testScope.runBlockingTest {
+        `when`(productRepository.invoke(1))
+            .thenReturn(flowOf(Resource.Success(Product(brand = "abc"), "Success")))
         productViewModel.getProductByID(1)
-        delay(1000)
         assertThat(productViewModel.productDataState.value.value.apiState).isEqualTo(ApiState.Success)
         assertThat(productViewModel.productDataState.value.value.product).isEqualTo(Product(brand = "abc"))
     }
 
     @Test
-    fun `test add to Cart`() = runBlocking {
-        `when`(productDao.insertProduct(Product(brand = "xyz"))).thenReturn(1)
+    fun `test add to Cart`() = testScope.runBlockingTest {
+        `when`(cartRepository.invoke(Product(brand = "xyz")))
+            .thenReturn(flowOf(Resource.Success(1, "Success")))
         productViewModel.addToCart(Product(brand = "xyz"))
-        delay(1000)
         assertThat(productViewModel.addToCartDataState.value.value.id).isEqualTo(1)
     }
 }
